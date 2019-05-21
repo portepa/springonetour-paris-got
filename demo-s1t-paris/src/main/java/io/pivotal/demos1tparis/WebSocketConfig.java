68e73bd16ca5b3945354697c7e3caef1a047acb2
Pierre-Antoine Porte <porte.pierreantoine@gmail.com>
2019-05-21 17:51:17 +0200

    change votes to Jon vs Daenerys

--- demo-s1t-paris/src/main/java/io/pivotal/demos1tparis/WebSocketConfig.java
+++ demo-s1t-paris/src/main/java/io/pivotal/demos1tparis/WebSocketConfig.java
@@ -27,7 +27,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/");
-        // config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
